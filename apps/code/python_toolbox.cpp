caabc15721cede459f9b4c73b32d72516d5adf6b
willem <willem@leit.so>
2019-11-03 14:31:30 +0100

    Added wait_vblank and get_keys to python toolbox

--- apps/code/python_toolbox.cpp
+++ apps/code/python_toolbox.cpp
@@ -100,7 +100,9 @@ const ToolboxMessageTree KandinskyModuleChildren[] = {
   ToolboxMessageTree::Leaf(I18n::Message::PythonCommandSetPixel, I18n::Message::PythonSetPixel),
   ToolboxMessageTree::Leaf(I18n::Message::PythonCommandColor, I18n::Message::PythonColor),
   ToolboxMessageTree::Leaf(I18n::Message::PythonCommandDrawString, I18n::Message::PythonDrawString),
-  ToolboxMessageTree::Leaf(I18n::Message::PythonCommandFillRect, I18n::Message::PythonFillRect)
+  ToolboxMessageTree::Leaf(I18n::Message::PythonCommandFillRect, I18n::Message::PythonFillRect),
+  ToolboxMessageTree::Leaf(I18n::Message::PythonCommandGetKeys, I18n::Message::PythonGetKeys),
+  ToolboxMessageTree::Leaf(I18n::Message::PythonCommandWaitVBlank, I18n::Message::PythonWaitVBlank)
 };
 
 const ToolboxMessageTree RandomModuleChildren[] = {
